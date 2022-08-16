4488f0f3981172f0f81afa2412933ca72f88e471
user <user@LAPTOP-T98OIO9I.localdomain>
2022-08-16 21:43:17 +0900

    chapter 12

--- mysite/photo/urls.py
+++ mysite/photo/urls.py
@@ -13,5 +13,24 @@ urlpatterns = [
 
     # /photo/photo/99/
     path('photo/<int:pk>/', views.PhotoDV.as_view(), name='photo_detail'),
+
+    # create/update/delete url
+    # /photo/album/add/
+    path('album/add/', views.AlbumPhotoCV.as_view(), name='album_add'),
+    # /photo/album/change/
+    path('album/change/', views.AlbumChangeLV.as_view(), name='album_change'),
+    # /photo/album/99/update/
+    path('album/<int:pk>/update/', views.AlbumPhotoUV.as_view(), name='album_update'),
+    # /photo/album/99/delete/
+    path('album/<int:pk>/delete/', views.AlbumDelV.as_view(), name='album_delete'),
+    
+    # /photo/photo/add/
+    path('photo/add/', views.PhotoCV.as_view(), name='photo_add'),
+    # /photo/photo/change/
+    path('photo/change/', views.PhotoChangeLV.as_view(), name='photo_change'),
+    # /photo/photo/99/update/
+    path('photo/<int:pk>/update/', views.PhotoUV.as_view(), name='photo_update'),
+    # /photo/photo/99/delete/
+    path('photo/<int:pk>/delete/', views.PhotoDelV.as_view(), name='photo_delete'),
 ]
 
